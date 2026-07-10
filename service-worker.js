9f57f27598e6f33424adc6b6526ee560712463ea
CaptBlistex <zfrck89yps@privaterelay.appleid.com>
2026-07-10 09:01:46 +0100

    Upgrade offline cache for v2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='reward-tracker-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='reward-tracker-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));});
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>cached)));});
